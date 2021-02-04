5ab535a0c8051f13db6e69467bd5356967855a3e
maifeeulasad <maifeeulasad@gmail.com>
2021-02-04 15:09:53 +0600

    removed babel-loader; removed unwanted files; added example component

---
+++ src/component/MyFirstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './some.css';
+
+class MyFirstComponent extends React.Component{
+
+	render(){
+		return(
+			<div className={"mycls"}>Love from MUA</div>		
+		)	
+	}
+
+}
+
+export default MyFirstComponent;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyFirstComponent from './component/MyFirstComponent'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyFirstComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
